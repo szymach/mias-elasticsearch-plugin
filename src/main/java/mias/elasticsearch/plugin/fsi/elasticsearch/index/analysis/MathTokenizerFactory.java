4cc6f8def75de549430194ff0af0de791652104c
Lukasz Cybula <lukasz@fsi.pl>
2022-07-15 10:02:48 +0200

    Simplifications

--- src/main/java/mias/elasticsearch/plugin/fsi/elasticsearch/index/analysis/MathTokenizerFactory.java
+++ src/main/java/mias/elasticsearch/plugin/fsi/elasticsearch/index/analysis/MathTokenizerFactory.java
@@ -4,23 +4,23 @@
  * For the full copyright and license information, please view the LICENSE file
  * that was distributed with this source code.
  */
-
 package mias.elasticsearch.plugin.fsi.elasticsearch.index.analysis;
 
+import cz.muni.fi.mias.math.MathTokenizer;
 import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.util.AttributeFactory;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.analysis.AbstractTokenizerFactory;
 
 public class MathTokenizerFactory extends AbstractTokenizerFactory {
-
     public MathTokenizerFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
         super(indexSettings, settings, name);
     }
 
     @Override
     public Tokenizer create() {
-        return new MathTokenizer();
+        return new MathTokenizer(AttributeFactory.DEFAULT_ATTRIBUTE_FACTORY, true, MathTokenizer.MathMLType.BOTH);
     }
 }
